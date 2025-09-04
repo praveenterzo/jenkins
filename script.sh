f9205a3e59f03b0571da5885ba6146e9bbeb1c74
praveenterzo <102901741+praveenterzo@users.noreply.github.com>
2025-09-04 16:20:14 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,2 @@
-#!/usr/bin/env bash
 set -euo pipefail
-echo "Run started: $(date)" | tee output.txt
-echo "Hello from Jenkins on commit: ${GIT_COMMIT:-unknown}" | tee -a output.txt
-uname -a | tee -a output.txt
-echo "Run finished: $(date)" | tee -a output
+echo "Hello from Jenkins! The time is: $(date)
